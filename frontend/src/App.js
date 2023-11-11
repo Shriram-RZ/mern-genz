402b0b730d9e5a2d7a00cd3944018bad7d9104b9
Shri ram <shriram173yt@gmail.com>
2023-11-12 01:05:17 +0530

    video-9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import data from "./data";
-import { BrowserRouter,Link,Route,Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Navbar } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
@@ -7,18 +10,28 @@ import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <header className="App-header">
-        <Link to="/">Gen-Z</Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen />} />
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
-        
-      </main>
-    </div>
+      <div className="d-flex flex-column site-container">
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Gen-Z</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
