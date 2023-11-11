d66fd0df9654a3f442e780b36884025b4a908153
Shri ram <shriram173yt@gmail.com>
2023-11-11 20:57:20 +0530

    video-5

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInstock: 10,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description : 'high quality product'
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category:'pants',
+            image:'/images/p4.png',
+            price: 65,
+            countInstock: 23,
+            brand:'Adidas',
+            rating: 4.7,
+            numReviews: 12,
+            description : 'high quality cloth'
+        },
+        {
+            name: 'Nike Slim shirt',
+            slug: 'mark-slim-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            countInstock: 10,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description : 'high quality product'
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'spencer-fit-pant',
+            category:'pants',
+            image:'/images/p4.png',
+            price: 65,
+            countInstock: 23,
+            brand:'Adidas',
+            rating: 4.7,
+            numReviews: 12,
+            description : 'high quality cloth'
+        }
+        
+    ]
+};
+export default data;
