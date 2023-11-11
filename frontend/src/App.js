35120ec893624cd2135c74bbd7e8864eb0aab220
Shri ram <shriram173yt@gmail.com>
2023-11-11 20:13:49 +0530

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Gen-Z</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
